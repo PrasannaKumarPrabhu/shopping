c3bd622249d30c8a3304ca421811d18acc3c88ae
prasannakumar2806 <prasannakumar.prabhu@hayagreeva.co.in>
2024-02-23 05:47:01 +0530

    test case

--- src/test/java/com/shopping/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/shopping/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,54 @@
 package com.shopping.productservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper; // Import ObjectMapper
+import com.shopping.productservice.dto.ProductRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:postgresql://localhost:5432/product-service",
+		"spring.datasource.username=postgres",
+		"spring.datasource.password=0000"
+})
+@Transactional
 class ProductServiceApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper; // Inject ObjectMapper
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+		String productRequestString = objectMapper.writeValueAsString(productRequest);
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(productRequestString)
+		).andExpect(status().isCreated());
+	}
+
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("Iphone 13")
+				.description("iphone 13")
+				.price(BigDecimal.valueOf(1200))
+				.build();
 	}
 
 }
